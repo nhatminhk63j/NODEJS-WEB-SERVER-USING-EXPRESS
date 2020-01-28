879c2fe1810f4c536e9f3c4d10b5a3211150f56c
Nhat Ngo Sach <nhatngo11a1@gmail.com>
2020-01-28 08:40:27 +0700

    Use Pug để view
    Các render các file html sử dụng pug và tìm hiểu về query tìm kiếm với form.

--- NodeJS1/index.js
+++ NodeJS1/index.js
@@ -2,6 +2,29 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-app.listen(port, () => console.log('App listening on port ${port}!'))
+var users = [
+    { id: 1, name: 'Pham Thi Lien' },
+    { id: 2, name: 'Ngo Sach Nhat' }
+]
+app.get('/', (req, res) => res.render('index', {
+    name: 'Express'
+}));
+
+app.get('/users', (req, res) => res.render('users/index', {
+    users: users
+}));
+
+app.get('/users/search', (req, res) => {
+    var q = req.query.q;
+    var matchedUsers = users.filter((user) => {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    })
+    res.render('users/index', {
+        users: matchedUsers
+    })
+});
+
+app.listen(port, () => console.log('App listening on port ' + port));
