e1de9988621810691453dc7d594b539d9ba80a3c
Nhat Ngo Sach <nhatngo11a1@gmail.com>
2020-01-28 09:10:05 +0700

    Post method and view req.body

--- NodeJS1/index.js
+++ NodeJS1/index.js
@@ -5,6 +5,9 @@ const port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 var users = [
     { id: 1, name: 'Pham Thi Lien' },
     { id: 2, name: 'Ngo Sach Nhat' }
@@ -26,5 +29,14 @@ app.get('/users/search', (req, res) => {
         users: matchedUsers
     })
 });
+
+app.get('/users/create', (req, res) => {
+    res.render('users/create');
+})
+
+app.post('/users/create', (req, res) => {
+    users.push(req.body);
+    res.redirect('/users');
+})
 
 app.listen(port, () => console.log('App listening on port ' + port));
